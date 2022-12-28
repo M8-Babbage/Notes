58f0bea1837ab88575d683040c7620b088759227
Edwin J. Páez <edwinjpr11@gmail.com>
2022-12-28 11:38:08 -0500

    feat: add overload examples

--- src/functions.ts
+++ src/functions.ts
@@ -30,4 +30,27 @@
 3. ${status ?? "Status null o undefined: Nullish coalescing"}`;
   };
   console.log(getOldUser("Laura"));
+
+  // --------------------------------------------------------------------//
+  // ---------------------------- OVERLOAD ------------------------------//
+  // --------------------------------------------------------------------//
+  // Laura => [L,a,u,r,a] => string => string[]
+  // [L,a,u,r,a] => Laura => string[] => string
+  function parseStr(input: string): string[];
+  function parseStr(input: string[]): string;
+  // TypeScript en este caso no podrá inferir el tipo de retorno con exactitud
+  // Por eso le hacemos la sobrecarga de las líneas de arriba
+  // Buenas practicas, si hay un any o un unkown en el overload se deja al final
+
+  function parseStr(input: unknown): unknown {
+    if (Array.isArray(input)) {
+      return input.join(""); // string
+    } else if (typeof input === "string") {
+      return input.split(""); // string[]
+    }
+  }
+  console.log(
+    `4. Overload: Array to String ${parseStr(["L", "a", "u", "r", "a"])}`
+  );
+  console.log(`5. Overload: String to Array ${parseStr("Laura")}`);
 })();
