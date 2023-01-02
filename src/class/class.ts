0c7cc1862814a777460fa1dea376e015f246d0b8
Edwin J. Páez <edwinjpr11@gmail.com>
2023-01-02 15:29:39 -0500

    chore: add new examples with class, abstract, implmements, extends

--- src/class/class.ts
+++ src/class/class.ts
@@ -3,16 +3,23 @@
 // --------------------------------------------------------------------//
 (() => {
   class Avenger {
-    static nombre: string; // Propiedad estática
-    public nombreReal: string; // Propiedad pública
-    private puedePelear: boolean; // Propiedad privada
-    protected test: string; // Propiedad protegida
+    // Encapsulamiento o tipo de acceso, se pueden añadir tanto a propiedades o atributos y a métodos
+    static nombre: string; // Propiedad estática, son propiedades que pertecen a la clase y no a las instancias, puedes ser llamados desde métodos estáticos
+    static readonly imStatic: string = "Laura"; // Propiedad estática que no puede ser reasignada
+    public nombreReal: string; // Propiedad pública, puede ser llamada en todas las instancias de la clase y pueden ser modificadas
+    private puedePelear: boolean; // Propiedad privada, solo puede ser llamada dentro de la clase
+    protected wifeName: string; // Propiedad protegida, solo la clase y clases heredadas pueden usar estos atributos
 
-    constructor(nombre: string, nombreReal: string, puedePelear: boolean, test?: string) {
+    constructor(
+      nombre: string,
+      nombreReal: string,
+      puedePelear: boolean,
+      test?: string
+    ) {
       Avenger.nombre = nombre; // Asignar una propiedad estática
       this.nombreReal = nombreReal; // Asignar una propiedad pública
       this.puedePelear = puedePelear; // Asignar una propiedad privada
-      this.test = test || 'test'; // Asignar una propiedad protegida, propiedad opcional
+      this.wifeName = test || "Susano"; // Asignar una propiedad protegida, propiedad opcional
     }
 
     // Métodos estáticos, solo pueden apuntar a otras propiedades estáticas
@@ -33,20 +40,25 @@
 
     // Los propiedades / métodos protegidos pueden ser usadas desde clases heredadas
     protected getFullName(): string {
-      return `${this.nombreReal} - ${this.test}`;
+      return `${this.nombreReal} - ${this.wifeName}`;
     }
   }
 
   // Instancia de la clase Avenger
-  const antman: Avenger = new Avenger('Antman C', 'Scott Lang', true, 'protegida');
+  const antman: Avenger = new Avenger(
+    "Antman C",
+    "Scott Lang",
+    true,
+    "protegida"
+  );
   // Modificar una propiedad pública
-  antman.nombreReal = 'Antman';
+  antman.nombreReal = "Antman";
   // Mostrar en pantalla los valores de las propiedades de la instancia antman
   console.log(antman);
   // Obtener el nombre estático de la clase
   console.log(Avenger.staticMethod());
   // Definir el nombre privado
-  antman.setName = 'Antman C';
+  antman.setName = "Antman C";
   // Obtener el nombre por medio de un método getter, no van los ()
   console.log(antman.getName);
 
@@ -54,14 +66,19 @@
   // --------------------------- HERENCIA -------------------------------//
   // --------------------------------------------------------------------//
   class Xmen extends Avenger {
-    constructor(nombre: string, nombreReal: string, puedePelear: boolean, test: string) {
+    constructor(
+      nombre: string,
+      nombreReal: string,
+      puedePelear: boolean,
+      test: string
+    ) {
       // En el súper se traen las propiedades que se heredan y también se pasan en el constructor
       super(nombre, nombreReal, puedePelear, test);
     }
 
     // Sobreescribir un método de la clase padre
     getFullName(): string {
-      return `${this.nombreReal} - ${this.test} - Xmen`;
+      return `${this.nombreReal} - ${this.wifeName} - Xmen`;
     }
 
     // Usar el mismo método de la clase padre: void porque no retorna nada, solo muestra
@@ -71,6 +88,6 @@
   }
 
   // Instancia de la clase Xmen
-  const newXmen: Xmen = new Xmen('Wolverine', 'Logan', true, 'protegida');
+  const newXmen: Xmen = new Xmen("Wolverine", "Logan", true, "protegida");
   newXmen.getFullNameXmen();
 })();
